52605c396582d044985abe7afd38b5de34b3bd05
mariaadannies <mariaadannies26@gmail.com>
2022-09-14 23:30:50 +0700

    lab2

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 from wishlist.views import show_wishlist
+from wishlist.views import show_wishlist_xml
+from wishlist.views import show_wishlist_json_id
 
 app_name = 'wishlist'
 
 urlpatterns = [
     path('', show_wishlist, name='show_wishlist'),
+    path('wishlist_xml/', show_wishlist_xml, name='show_wishlist_xml'),
+    path('wishlist_json_id/<int:id>', show_wishlist_json_id, name='show_wishlist_json_id')
 ]
--- wishlist/views.py
+++ wishlist/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render
 from wishlist.models import BarangWishlist
+from django.http import HttpResponse
+from django.core import serializers
 
 def show_wishlist(request):
     return render(request, "wishlist.html",context)
+
+def show_wishlist_xml(request):
+    data = BarangWishlist.objects.all()
+
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
+def show_wishlist_json_id(request):
+    data = BarangWishlist.objects.filter(pk=id)
+
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
 
 data_barang_wishlist = BarangWishlist.objects.all()
 context = {
