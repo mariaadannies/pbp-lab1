303a7c768edf769b0f9464bd45edeb2c0dff960a
mariaadannies <mariaadannies26@gmail.com>
2022-09-22 12:54:25 +0700

    lab3

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -3,6 +3,9 @@ from wishlist.views import show_wishlist
 from wishlist.views import show_wishlist_xml
 from wishlist.views import show_wishlist_json
 from wishlist.views import show_wishlist_json_id
+from wishlist.views import register 
+from wishlist.views import login_user 
+from wishlist.views import logout_user 
 
 app_name = 'wishlist'
 
@@ -10,5 +13,8 @@ urlpatterns = [
     path('', show_wishlist, name='show_wishlist'),
     path('xml/', show_wishlist_xml, name='show_wishlist_xml'),
     path('json/', show_wishlist_json, name='show_wishlist_json'),
-    path('json/<int:id>', show_wishlist_json_id, name='show_wishlist_json_id')
+    path('json/<int:id>', show_wishlist_json_id, name='show_wishlist_json_id'),
+    path('register/', register, name='register'),
+    path('login/', login_user, name='login'),
+    path('logout/', logout_user, name='logout')
 ]
